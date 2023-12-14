fd18aadfe4fcb799be8db173ea84e0873a14dbac
Ameya <joshiameyajoshi@gmail.com>
2023-12-14 00:07:30 -0600

    Meme Generator: Create Vite react app, render Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <header className="header">
+            <img className="header--img" src='/public/images/troll-face.png' alt="meme" ></img>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--text">React Course - Project 3</h4>
+        </header>
+    );
+}
+
