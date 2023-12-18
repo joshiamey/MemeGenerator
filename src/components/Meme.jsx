db07b4e5c38a05164bce5370ed5741c7513ed9e5
Ameya <joshiameyajoshi@gmail.com>
2023-12-18 14:02:50 -0600

    Added use of react state logic to update meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData";
+
 
 const Meme = () => {
+    const [memeImg,setMemeImg] = useState("");
+
+    function getNewImage() {
+        const memes = memesData.data.memes;
+        const randomMemeIndex = Math.floor(Math.random() * memes.length);
+        setMemeImg(prevMemeImg => {
+            console.log('Previous url = ',prevMemeImg, ' setting to new url= ',memes[randomMemeIndex].url);
+            setMemeImg(memes[randomMemeIndex].url);
+        })
+    }
+
+
     return (
         <div className="meme--container">
-          <form className="top--form">
-            <input className="top--text" placeholder="Top Text" type="text"></input>
-            <input className="bottom--text" placeholder="Bottom Text" type="text"></input>
-            <button className="meme--button">Get a new meme image  🖼</button>
-          </form>
+          <div className="meme--form">
+            <div>
+                <input className="top--text" placeholder="Shut up" type="text"></input>
+            </div>
+            <div>
+                <input className="bottom--text" placeholder="and take my money" type="text"></input>
+            </div>
+            <button className="meme--button" onClick={getNewImage}>Get a new meme image  🖼</button>
+          </div>
+          <img className="meme--img" src={memeImg}></img>
         </div>
     );
 };
