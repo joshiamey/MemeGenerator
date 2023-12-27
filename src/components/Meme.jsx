93f314dbac7728c71a419818e3a63e88dea73796
Ameya <joshiameyajoshi@gmail.com>
2023-12-27 02:11:29 -0600

    Added logic for updating the text on the image from the input fields by adding onChange event listener to the input fields and updating state values

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -22,18 +22,50 @@ const Meme = () => {
         })
     }
 
+    function handleChange(event) {
+        const {name,value} = event.target
+
+        // Instead of returning , open the body of function, a different
+        // way of modifying meme state
+        setMeme(prevMeme => (
+            {                
+                ...prevMeme,
+                [name]: value                
+            }
+        ))
+    }
+
 
     return (
         <div className="meme--container">
           <div className="meme--form">
-                <input className="form--input" placeholder="Top Text" type="text"></input>
-                <input className="form--input" placeholder="Bottom text" type="text"></input>
-            <button className="meme--button" onClick={getNewImage}>Get a new meme image  🖼</button>
+            <input 
+                className="form--input" 
+                placeholder="Top Text" 
+                type="text" 
+                name="topText"
+                value={meme.topText}
+                onChange={handleChange}
+                />
+            <input 
+                className="form--input" 
+                placeholder="Bottom text" 
+                type="text" 
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleChange}
+                />
+            <button 
+                className="meme--button" 
+                onClick={getNewImage}
+                >
+                Get a new meme image  🖼
+            </button>
           </div>
           <div className="meme">
             <img className="meme--img" src={meme.randomImg}></img>
-            <h2 className="meme--text top">One does not simply</h2>
-            <h2 className="meme--text bottom">Walk into Mordor</h2>
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
           </div>
         </div>
     );
