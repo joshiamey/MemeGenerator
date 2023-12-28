4863d38b4542d4156988e21b94239279864b0273
Ameya <joshiameyajoshi@gmail.com>
2023-12-28 02:30:42 -0600

    Moved fetching to its own function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -10,13 +10,16 @@ const Meme = () => {
 
     const [allMemes, setAllMemes] = React.useState([]);
     
+    function fetchMemes() {
+        fetch(MEMES_API_URI)
+        .then(resp => resp.json())
+        .then(data => setAllMemes(data.data.memes));
+    }
     // Using effect to fetch the memes data from external URL and populate allmemes array
     // as we want to run it once , use empty dependency list
     useEffect(() => {
-        console.log("Fetched all Memes.")
-        fetch(MEMES_API_URI)
-        .then(resp => resp.json())
-        .then(data => setAllMemes(data.data.memes))
+        console.log("Fetched all Memes.");
+        fetchMemes();
     },[])
     
 
